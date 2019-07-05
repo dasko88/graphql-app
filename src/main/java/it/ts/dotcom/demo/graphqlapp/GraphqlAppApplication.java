5bed708cf169f421c9c21a7befbd208cc2dab520
David Škorjanc <dskorjanc@dotcom.ts.it>
2019-07-05 14:31:36 +0200

    Added data.sql

--- src/main/java/it/ts/dotcom/demo/graphqlapp/GraphqlAppApplication.java
+++ src/main/java/it/ts/dotcom/demo/graphqlapp/GraphqlAppApplication.java
@@ -1,43 +1,13 @@
 package it.ts.dotcom.demo.graphqlapp;
 
-import it.ts.dotcom.demo.graphqlapp.model.entity.Task;
-import it.ts.dotcom.demo.graphqlapp.model.entity.User;
-import it.ts.dotcom.demo.graphqlapp.model.repository.TaskRepository;
-import it.ts.dotcom.demo.graphqlapp.model.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-import java.util.Arrays;
-import java.util.Date;
 
 @SpringBootApplication
 public class GraphqlAppApplication {
 
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private TaskRepository taskRepository;
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqlAppApplication.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		userRepository.saveAll(Arrays.asList(
-				new User(null, "Mario", "Verdi", 193, new Date()),
-				new User(null, "Frank", "Rossi", 168, new Date()),
-				new User(null, "Roberto", "Mancini", 174, new Date())
-		));
-		taskRepository.saveAll(Arrays.asList(
-				new Task(null, "Pulire casa", "C001", 1, new Date()),
-				new Task(null, "Fare spesa", "C002", 2, new Date()),
-				new Task(null, "Ascoltare musica", "C003", 1, new Date()),
-				new Task(null, "Guardare film", "C004", 3, new Date())
-		));
 	}
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user(name, surname, height, created_date) values ('Mario','Rossi',192, current_timestamp);
+insert into user(name, surname, height, created_date) values ('Franck','La Furia',174, current_timestamp);
+insert into user(name, surname, height, created_date) values ('Luca','Topolino',183, current_timestamp);
+
+insert into task(name, code, priority, due_date) values ('Lavare','C001',1, current_timestamp);
+insert into task(name, code, priority, due_date) values ('Ascoltare musica','C002',2, current_timestamp);
+insert into task(name, code, priority, due_date) values ('Dormire','C003',3, current_timestamp);
+insert into task(name, code, priority, due_date) values ('Mangiare','C004',1, current_timestamp);
